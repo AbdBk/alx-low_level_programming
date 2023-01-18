ca214b713e0742d96953858301b59d32a3d4e7d7
AbdBk <mabdiyah@gmail.com>
2023-01-19 02:26:07 +0300

    Task 0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,17 @@
-#include <stdio.h>
 #include "function_pointers.h"
-/**
- * print_name - function thats prints a name passed to it
- * @name: char to display to the stdout
- * @f: A pointer function
- * Return: No return cause we've declared VOID as R_TYPE
-*/
-void print_name(char *name, void (*f)(char *))
-{
-	if (!name || !f) /* if any is NULL */
-		return;
+	#include <stdio.h>
+	/**
+	 * print_name - print name using pointer to function
+	 * @name: string to add
+	 * @f: pointer to function
+	 * Return: nothing
+	 **/
+	void print_name(char *name, void (*f)(char *))
+	{
+		if (name == NULL || f == NULL)
+			return;
 
-	f(name);
-}
+
+		f(name);
+	}
+
