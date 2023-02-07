8af449b6ebbcbf084a6ee95c6b90d7163b4e6731
AbdBk <mabdiyah@gmail.com>
2023-02-07 04:09:54 +0300

    Task 1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,20 +1,30 @@
 #include "main.h"
 /**
- * main - check the code
- * @ac: arguments to main
- * @av: arguments to main
- * Return: Always 0.
+ * create_file - creates a file and adds permission to it
+ * @filename: The name of the file to be created
+ * @text_content: content to be placed in the created file
+ * Return: 1 if successfull and -1 on err.
  */
-int main(int ac, char **av)
+int create_file(const char *filename, char *text_content)
 {
-	int res;
+	ssize_t fild = 0, chk = 0, i = 0;
 
-	if (ac != 3)
-	{
-		dprintf(2, "Usage: %s filename text\n", av[0]);
-		exit(1);
-	}
-	res = create_file(av[1], av[2]);
-	printf("-> %i)\n", res);
-	return (0);
+	if (!filename)
+		return (-1);
+
+	if (!text_content)
+		text_content = "";
+
+	fild = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
+	if (fild < 0)
+		return (-1);
+
+	while (text_content[i])
+		i++;
+	chk = write(fild, text_content, i);
+	if (chk < 0)
+		return (-1);
+
+	close(fild);
+	return (1);
 }
